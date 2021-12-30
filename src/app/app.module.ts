31287b4af26db3707b7bc9f6cdea4bf44ce2629d
nicolasMorzan <nicomorzan1993@gmail.com>
2021-12-30 17:24:44 -0300

    skeleton finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkeletonComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
